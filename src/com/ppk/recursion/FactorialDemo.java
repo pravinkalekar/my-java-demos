87cf74723cf9748d5ed999599648c449fafd1a3e
Pravin Kalekar <pravin.kalekar@gmail.com>
2019-08-01 15:59:26 -0700

    Adding factorial using plain iteration

--- src/com/ppk/recursion/FactorialDemo.java
+++ src/com/ppk/recursion/FactorialDemo.java
@@ -1,20 +1,48 @@
 package com.ppk.recursion;
 
 /**
- * Factorial of a given number using recursion
- * 
- * Factorial(x) = x * Factorial(x-1)
+ * Factorial of a given number
+ * <br>
+ * 1. Plain Iteration way
+ * <br>
+ * 2. Recursion
+ * <br>
+ * 3. Tail Recursion
  * 
  * @author pkalekar
  *
  */
 public class FactorialDemo {
 	
-	public static int factorial(int number) {
+	
+	/**
+	 * Factorial using iteration
+	 * 
+	 * Factorial(x) = x * x-1 * x-2 * .. * 3 * 2 * 1
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static int factorialIteration(int number) {
+		
+		int result = 1;
+		for (int i = number; i > 0; i--) {
+			result *= i;
+		}
+		return result;
+	}
+	
+	/**
+	 * Factorial(x) = x * Factorial(x-1)
+	 * 
+	 * @param number
+	 * @return
+	 */
+	public static int factorialRecursion(int number) {
 		//breaking condition
 		if(number == 0) return 1;
 		
-		return number * factorial(number-1);
+		return number * factorialRecursion(number-1);
 	}
 	
 	
@@ -50,9 +78,11 @@ public class FactorialDemo {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Factorial(5)="+factorial(5));
+		System.out.println("factorialIteration(5)="+factorialIteration(5));
 		
-		System.out.println("Factorial(6)="+factorialUsingTailRecursion(6));
+		System.out.println("factorialRecursion(5)="+factorialRecursion(5));
+		
+		System.out.println("factorialUsingTailRecursion(5)="+factorialUsingTailRecursion(5));
 	}
 
 }
