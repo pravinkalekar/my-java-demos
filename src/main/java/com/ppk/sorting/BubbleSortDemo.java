290ffa68d1bc198d03a0bf60260b01da8a53b13f
Pravin Kalekar <pravin.kalekar@gmail.com>
2019-08-12 00:02:39 -0700

    Added Optimized Version of Bubble Sort

--- src/main/java/com/ppk/sorting/BubbleSortDemo.java
+++ src/main/java/com/ppk/sorting/BubbleSortDemo.java
@@ -15,12 +15,19 @@ import java.util.Arrays;
  * compares each pair of adjacent items and swaps them if they are in the wrong order. 
  * The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.
  * 
+ * 
  * @author pkalekar
- *
  */
 public class BubbleSortDemo {
 
 	
+	/**
+	 * Time Complexity (Worst) - O( n^2 )
+	 * <br>
+	 * Time Complexity (Best) - O( n^2 )
+	 * 
+	 * @param data
+	 */
 	public static void bubbleSort(int[] data) {
 		//first loop runs for array length minus one i.e. 4 times for 5 element array
 		for (int i = 0; i < (data.length -1) ; i++) {
@@ -35,12 +42,50 @@ public class BubbleSortDemo {
 		}
 	}
 	
+	/**
+	 * Time Complexity (Worst) - O( n^2 )
+	 * <br>
+	 * Time Complexity (Best) - O( n )
+	 * <p>
+	 * 
+	 * @param data
+	 */
+	public static void bubbleSortOptimized(int[] data) {
+		//first loop runs for array length minus one i.e. 4 times for 5 element array
+		for (int i = 0; i < (data.length -1) ; i++) {
+			boolean swapped = false;
+			//second loop runs for i less than first loop
+			for (int j = 0; j < (data.length -1) - i ; j++) {
+				if(data[j] > data[j+1]) {
+					int temp = data[j];
+					data[j] = data[j+1];
+					data[j+1] = temp;
+					
+					swapped = true;
+				}
+			}
+			
+			if(!swapped) {
+				break;
+			}
+			
+		}
+	}
+	
 	public static void main(String[] args) {
 		int[] a = {4,3,6,4,43,1};
 		
 		System.out.println("Before - "+ Arrays.toString(a));
 		
 		bubbleSort(a);
+		
+		System.out.println("After - "+ Arrays.toString(a));
+		
+		a = new int[]{4,3,6,4,43,1};
+		
+		System.out.println("Before - "+ Arrays.toString(a));
+		
+		bubbleSortOptimized(a);
 		
 		System.out.println("After - "+ Arrays.toString(a));
 	}
